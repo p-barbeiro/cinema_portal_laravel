0e6e42b6885c7b0606c378dd43d2479fb525c769
p-barbeiro <pedrobarbeiro7@gmaill.com>
2024-05-05 19:15:23 +0100

    add models

---
+++ app/Models/Screening.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Screening extends Model
+{
+    use HasFactory;
+
+    public function theater(): BelongsTo
+    {
+        return $this->belongsTo(Theater::class);
+    }
+
+    public function movie(): BelongsTo
+    {
+        return $this->belongsTo(Movie::class);
+    }
+
+    public function tickets(): HasMany
+    {
+        return $this->hasMany(Ticket::class);
+    }
+}
---
+++ app/Models/User.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class User extends Model
+{
+    use HasFactory;
+
+    public $timestamps = false;
+
+    public function customer(): HasOne
+    {
+        return $this->hasOne(Customer::class);
+    }
+
+    public function theater(): BelongsTo
+    {
+        return $this->belongsTo(Theater::class);
+    }
+}
