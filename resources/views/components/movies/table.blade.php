26e2c317e47c780ca7752d370cb51f48b14acd3e
p-barbeiro <pedrobarbeiro7@gmaill.com>
2024-06-03 01:03:56 +0100

    movies finished

--- resources/views/components/movies/table.blade.php
+++ resources/views/components/movies/table.blade.php
@@ -6,25 +6,27 @@
             <th class="px-2 py-2 text-center">Name</th>
             <th class="px-2 py-2 text-center hidden sm:table-cell">Genre</th>
             <th class="px-2 py-2 text-center hidden sm:table-cell">Year</th>
-            <th></th>
+            <th class="hidden sm:table-cell"></th>
             @if($showView)
-                <th></th>
+                <th class="hidden sm:table-cell"></th>
             @endif
             @if($showEdit)
-                <th></th>
+                <th class="hidden sm:table-cell"></th>
             @endif
             @if($showDelete)
-                <th></th>
+                <th class="hidden sm:table-cell"></th>
             @endif
         </tr>
         </thead>
         <tbody>
         @foreach ($movies as $movie)
-            <tr class="border-b border-b-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 dark:border-b-gray-500">
+            <tr class="border-b border-b-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 dark:border-b-gray-500 hover:bg-gray-50">
+
                 <td class="px-2 py-2 w-32">
                     <img class="object-center aspect-auto w-32 h-40 rounded" src="{{ $movie->getPoster() }}"
                          alt="{{ $movie->title }}">
                 </td>
+
                 <td class="px-2 py-2 text-left text-justify">
                     <div class="text-lg">
                         <a href="{{ route('movies.show', ['movie' => $movie]) }}" class="hover:font-bold hover:underline">
@@ -35,45 +37,52 @@
                         {{$movie->synopsis}}
                     </div>
                 </td>
+
                 <td class="px-2 py-2 text-left hidden sm:table-cell">{{ $movie->genre->name }}</td>
+
                 <td class="px-2 py-2 text-right hidden sm:table-cell">{{ $movie->year }}</td>
-                <td>
+
+                <td class="hidden sm:table-cell">
                     @if($movie?->trailer_url)
                         <x-table.icon-trailer class="ps-3 px-0.5" href="{{ $movie?->trailer_url }}"/>
                     @else
                         <x-table.icon-trailer class="ps-3 px-0.5" :trailer="false" />
                     @endif
                 </td>
+
                 @if($showView)
                     @can('view', $movie)
-                        <td>
+                        <td class="hidden sm:table-cell">
                             <x-table.icon-show class="ps-3 px-0.5"
                                                href="{{ route('movies.show', ['movie' => $movie]) }}"/>
                         </td>
                     @else
-                        <td></td>
+                        <td class="hidden sm:table-cell"></td>
                     @endcan
                 @endif
+
                 @if($showEdit)
                     @can('update', $movie)
-                        <td>
+                        <td class="hidden sm:table-cell">
                             <x-table.icon-edit class="px-0.5"
                                                href="{{ route('movies.edit', ['movie' => $movie]) }}"/>
                         </td>
                     @else
-                        <td></td>
+                        <td class="hidden sm:table-cell"></td>
                     @endcan
                 @endif
+
                 @if($showDelete)
                     @can('delete', $movie)
-                        <td>
+                        <td class="hidden sm:table-cell">
                             <x-table.icon-delete class="px-0.5"
                                                  action="{{ route('movies.destroy', ['movie' => $movie]) }}"/>
                         </td>
                     @else
-                        <td></td>
+                        <td class="hidden sm:table-cell"></td>
                     @endcan
                 @endif
+
             </tr>
         @endforeach
         </tbody>
--- resources/views/movies/edit.blade.php
+++ resources/views/movies/edit.blade.php
@@ -1,7 +1,5 @@
 @extends('layouts.main')
 
-@section('header-title', $movie->title)
-
 @section('main')
 <div class="flex flex-col space-y-6">
     <div class="p-4 sm:p-8 bg-white dark:bg-gray-900 shadow sm:rounded-lg">
