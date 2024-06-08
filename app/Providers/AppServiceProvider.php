fc49f06a5708d5bb16898bb8ed02b3528c6fb168
Joao Parreira <joao_parreira@mail.telepac.pt>
2024-06-09 00:03:20 +0100

    basically delete

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,7 +6,6 @@ use App\Models\Movie;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\Facades\Gate;
-use App\Models\Course;
 use App\Models\User;
 use App\Policies\AdministrativePolicy;
 use App\Policies\UserPolicy;
